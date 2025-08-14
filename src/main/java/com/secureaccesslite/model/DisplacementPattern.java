8866d44a10156c6eb5536d203bd241c604bf5694
Iam Arias <fabricioayaranga@gmail.com>
2025-08-14 11:09:11 -0500

    feat: Create initial domain model classes (User, Credential, DisplacementPattern)

--- src/main/java/com/secureaccesslite/model/DisplacementPattern.java
+++ src/main/java/com/secureaccesslite/model/DisplacementPattern.java
@@ -1,4 +1,40 @@
 package com.secureaccesslite.model;
 
+import java.util.List;
+
 public class DisplacementPattern {
+
+    private String patternId;
+    private String description;
+    private List<String> keyPoints;
+
+    public DisplacementPattern(String patternId, String description, List<String> keyPoints) {
+        this.patternId = patternId;
+        this.description = description;
+        this.keyPoints = keyPoints;
+    }
+
+    public String getPatternId() {
+        return patternId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public List<String> getKeyPoints() {
+        return keyPoints;
+    }
+
+    public void setPatternId(String patternId) {
+        this.patternId = patternId;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setKeyPoints(List<String> keyPoints) {
+        this.keyPoints = keyPoints;
+    }
 }
--- src/main/java/com/secureaccesslite/model/User.java
+++ src/main/java/com/secureaccesslite/model/User.java
@@ -1,4 +1,48 @@
 package com.secureaccesslite.model;
 
 public class User {
+
+    private String id;
+    private String name;
+    private String dni;
+    private String photoPath;
+
+    public User(String id, String name, String dni, String photoPath) {
+        this.id = id;
+        this.name = name;
+        this.dni = dni;
+        this.photoPath = photoPath;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public String getPhotoPath() {
+        return photoPath;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public void setPhotoPath(String photoPath) {
+        this.photoPath = photoPath;
+    }
 }
